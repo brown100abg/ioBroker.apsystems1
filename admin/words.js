4d96e1bb49961243e967443271b070e2ad88f922
ioBroker Adapter Creator <noreply@iobroker.net>
2025-07-06 07:05:30 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,33 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'apsystems1 adapter settings': {
+        'en': 'Adapter settings for apsystems1',
+        'de': 'Adaptereinstellungen für apsystems1',
+        'ru': 'Настройки адаптера для apsystems1',
+        'pt': 'Configurações do adaptador para apsystems1',
+        'nl': 'Adapterinstellingen voor apsystems1',
+        'fr': "Paramètres d'adaptateur pour apsystems1",
+        'it': "Impostazioni dell'adattatore per apsystems1",
+        'es': 'Ajustes del adaptador para apsystems1',
+        'pl': 'Ustawienia adaptera dla apsystems1',
+        'uk': 'Налаштування адаптера для apsystems1',
+        'zh-cn': 'apsystems1的适配器设置'
+    },
+    'host': {
+        'en': 'host',
+        'de': 'host',
+        'ru': 'host',
+        'pt': 'host',
+        'nl': 'host',
+        'fr': 'host',
+        'it': 'host',
+        'es': 'host',
+        'pl': 'host',
+        'uk': 'host',
+        'zh-cn': 'host'
+    }
+};
